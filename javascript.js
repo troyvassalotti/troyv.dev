36179b93e9870a659e5ac23bb4745b7f76959684
Troy Vassalotti <troyvassalotti@gmail.com>
2019-05-09 20:30:22 -0400

    testing body push

--- javascript.js
+++ javascript.js
@@ -1,12 +1,3 @@
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
 function openNav() {
     document.getElementById("sideNavigation").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
