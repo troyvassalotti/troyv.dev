8ef4f340a9d4cbda27653e6a36801687efaafb7f
Troy Vassalotti <troyvassalotti@gmail.com>
2019-05-09 20:27:54 -0400

    getting to understand how to make a mobile navigation

--- javascript.js
+++ javascript.js
@@ -6,3 +6,13 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
