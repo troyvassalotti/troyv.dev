e4e807b60511dec9c451364431611418e0f9023d
Troy Vassalotti <troyvassalotti@gmail.com>
2019-05-11 08:39:28 -0400

    Added a fade to the nav button

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("sideNavigation").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("icoOpen").classList.toggle("fade");
 }
 
 function closeNav() {
     document.getElementById("sideNavigation").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("icoOpen").classList.toggle("fade")
 }
