00db3a63f7adef571148afdad099e663f835ad26
Troy Vassalotti <troyvassalotti@gmail.com>
2019-11-03 15:28:10 -0500

    Setting up a test environment for javascript

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("icoOpen").classList.toggle("fade");
 }
-
 function closeNav() {
     document.getElementById("sideNavigation").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
--- super-secret/script.js
+++ super-secret/script.js
@@ -1,5 +1,4 @@
 /*
-
 JQuery Core
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
@@ -13,5 +12,15 @@ JQuery Mobile
 
 Bootstrap
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
-
 */
+
+var easterEgg1 = document.querySelector("#easterEgg1");
+function random(number) {
+  return Math.floor(Math.random()*(number+1));
+};
+easterEgg1.onclick = function() {
+  var rndCol1 = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  var rndCol2 = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.querySelector(".button").style.backgroundColor = rndCol1;
+  document.querySelector(".button").style.color = rndCol2;
+}
