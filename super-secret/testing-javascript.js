1cc977652d794ea845a891b0c9528d5debf1e145
Troy Vassalotti <troyvassalotti@gmail.com>
2019-11-06 19:41:27 -0500

    Easter egg now has transition time

--- super-secret/testing-javascript.js
+++ super-secret/testing-javascript.js
@@ -2,22 +2,35 @@
 const sideNav = document.getElementById("sideNavigation");
 const main = document.getElementById("main");
 const icon = document.getElementById("icoOpen");
+var openCount = 0, closeCount = 0;
 function openNav() {
     sideNav.style.width = "250px";
     main.style.marginLeft = "250px";
     icon.classList.toggle("fade");
+    openCount++;
+    console.log("Navigation opened " + openCount + " times.");
 }
 function closeNav() {
     sideNav.style.width = "0";
     main.style.marginLeft = "0";
-    icon.classList.toggle("fade")
+    icon.classList.toggle("fade");
+    closeCount++;
+    console.log("Navigation closed " + closeCount + " times.");
 }
+// create a new function to call openNav when closeNav has been called at least once and the viewport gets higher than 850px.
 
-// create a function that counts the number of times closeNav() happens
-// then use that number below to display the navbar when changing view sizes on desktop
-// if (closeNav() && document.getElementById("topNav").style.display === "none") {
-//   openNav();
-// };
+/*
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    main.style.marginLeft = 0;
+    openNav();
+  };
+};
+
+var x = window.matchMedia("(min-width: 850px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state change
+*/
 
 
 // change the color of all buttons at once
@@ -32,5 +45,8 @@ easterEgg1.onclick = function() {
   buttons.forEach(function(btn) {
     btn.style.backgroundColor = rndCol1;
     btn.style.color = rndCol2;
+    btn.style.transition = ".3s " + "linear";
   });
 };
+// NOTE: this changes inline css - need to think about creating classes for each color combo instead of variables.
+// create color pairings from my palette, assign them to variables, create a new function to switch through them.
