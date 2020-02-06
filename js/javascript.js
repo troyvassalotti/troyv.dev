27047f51533b2e3264ee4afcf726e7d19097420d
Troy Vassalotti <troyvassalotti@gmail.com>
2020-02-06 06:52:43 -0500

    Added recursive beta, and some new folders

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,8 @@
 // open and close the side navigation on mobile
 const sideNav = document.getElementById("sideNavigation");
 const main = document.getElementById("main");
-const open = document.getElementById("icoOpen");
-const close = document.getElementById("icoClose");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
 var openCount = 0, closeCount = 0;
 function openNav() {
     sideNav.classList.toggle("openNavSide");
