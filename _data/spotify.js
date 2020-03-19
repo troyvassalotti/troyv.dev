b8170e68d916f8c4e19f156206338ebbb0677ba5
Troy Vassalotti <troyvassalotti@gmail.com>
2020-03-19 19:34:57 -0400

    tried readjusting myself

--- _data/spotify.js
+++ _data/spotify.js
@@ -1,7 +1,6 @@
 const axios = require("axios").default;
 
 // https://developer.spotify.com/
-// figure out where to host the spotify api page
 module.exports = async function() {
   try {
     const response = await axios.get("https://api.spotify.com/v1/users/1222462159");
