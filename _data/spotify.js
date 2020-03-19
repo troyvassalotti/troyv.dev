ce2085e7ce4404b54f39563384fa23addd497341
Troy Vassalotti <troyvassalotti@gmail.com>
2020-03-19 19:44:49 -0400

    Update spotify.js
    commented out the zios code for now in the spotify js

--- _data/spotify.js
+++ _data/spotify.js
@@ -1,11 +1,10 @@
-const axios = require("axios").default;
-
 // https://developer.spotify.com/
-module.exports = async function() {
-  try {
-    const response = await axios.get("https://api.spotify.com/v1/users/1222462159");
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-}
+// const axios = require("axios").default;
+// module.exports = async function() {
+//   try {
+//     const response = await axios.get("https://api.spotify.com/v1/users/1222462159");
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
