1f3b1c5cca9100f32ca7e61388a325d207403aed
Troy Vassalotti <troyvassalotti@gmail.com>
2020-03-24 10:34:42 -0400

    Revert "Moved the favicon into the images folder"
    This reverts commit d8277ffbcc0c9318a0453524d57aed6db3f83c30.

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addPassthroughCopy("404.html");
   eleventyConfig.addPassthroughCopy("contact/resume.pdf");
+  eleventyConfig.addPassthroughCopy("favicon.png");
 
   eleventyConfig.addShortcode("mainNav", function() {
     return `<header>
