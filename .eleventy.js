b1c822e56aee2ad89abbaec47f7cd42a26ba0624
Troy Vassalotti <troyvassalotti@gmail.com>
2020-03-24 20:38:20 -0400

    changes made to 404 templates
    Success page changed template name and file extension.
    
    404 page changed file extension, made into a template, and deleted general 404.html.
    
    changed Eleventy.js and eleventyignore.

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("robots.txt");
-  eleventyConfig.addPassthroughCopy("404.html");
   eleventyConfig.addPassthroughCopy("contact/resume.pdf");
   eleventyConfig.addPassthroughCopy("favicon.png");
 
