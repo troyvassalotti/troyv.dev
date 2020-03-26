7c52a6b87a1510124ca0f0380f474302903f4256
Troy Vassalotti <troyvassalotti@gmail.com>
2020-03-26 16:29:57 -0400

    Made a11y changes to the homepage

--- .eleventy.js
+++ .eleventy.js
@@ -21,11 +21,11 @@ module.exports = function(eleventyConfig) {
       <nav class="sideNavigation" id="sideNavigation">
         <button id="close" aria-label="Close the navigation menu">&times;</button>
         <ul class="navbar">
-          <li id="link__home-container"><a href="/" aria-label="Go back to the homepage." id="link__home">troy vassalotti.</a></li>
-          <li id="link__blog-container"><a href="/blog/" id="link__blog">blog.</a></li>
-          <li id="link__projects-container"><a href="/projects/" id="link__projects">projects.</a></li>
-          <li id="link__music-container"><a href="/music/" id="link__music">music.</a></li>
-          <li id="link__contact-container"><a href="/contact/" id="link__contact">contact.</a></li>
+          <li id="link__home-container"><a href="/" aria-label="Go to the homepage." id="link__home">troy vassalotti.</a></li>
+          <li id="link__blog-container"><a href="/blog/" aria-label="Visit my blog." id="link__blog">blog.</a></li>
+          <li id="link__projects-container"><a href="/projects/" aria-label="View my various projects." id="link__projects">projects.</a></li>
+          <li id="link__music-container"><a href="/music/" aria-label="Learn more about my music." id="link__music">music.</a></li>
+          <li id="link__contact-container"><a href="/contact/" aria-label="View my resume and more on my contact page." id="link__contact">contact.</a></li>
         </ul>
       </nav>
     </header>`
@@ -39,10 +39,10 @@ module.exports = function(eleventyConfig) {
           <img id="me" src="/images/headshot-arrow_160x160.png" alt="This is me. I look like this." loading="lazy">
         </picture>
         <div class="footer-icons">
-          <a class="icon-link" href="https://github.com/troyvassalotti" target="_blank" rel="noopener" aria-label="Visit my GitHub."><img src="/images/github-black.svg" alt="Github"></a>
-          <a class="icon-link" href="https://codepen.io/troyvassalotti" target="_blank" rel="noopener" aria-label="Visit my CodePen."><img src="/images/codepen-black.svg" alt="Codepen"></a>
+          <a class="icon-link" href="https://github.com/troyvassalotti" target="_blank" rel="noopener" aria-label="Visit my GitHub page."><img src="/images/github-black.svg" alt="Github"></a>
+          <a class="icon-link" href="https://codepen.io/troyvassalotti" target="_blank" rel="noopener" aria-label="View smaller projects on my CodePen."><img src="/images/codepen-black.svg" alt="Codepen"></a>
           <a class="icon-link" href="https://www.linkedin.com/in/troy-vassalotti-468053142/" target="_blank" rel="noopener" aria-label="Visit my LinkedIn."><img src="/images/linkedin-black.svg" alt="Linkedin"></a>
-          <a class="icon-link" href="https://twitter.com/lowercasetroy" target="_blank" rel="noopener" aria-label="Visit my Twitter."><img src="/images/twitter-black.svg" alt="Twitter"></a>
+          <a class="icon-link" href="https://twitter.com/lowercasetroy" target="_blank" rel="noopener" aria-label="Follow me on Twitter."><img src="/images/twitter-black.svg" alt="Twitter"></a>
         </div>
       </div>
       <p>&copy; troy vassalotti.</p>
