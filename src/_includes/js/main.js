0ce9dc20959717a784a6054aa335ed76252f53c7
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-04-29 19:05:52 -0400

    confetti time

--- src/_includes/js/main.js
+++ src/_includes/js/main.js
@@ -1,1 +1,49 @@
 document.body.classList.add("js");
+
+// import * as ConfettiGenerator from "/assets/js/confetti.js";
+
+import confettiJs from "https://cdn.skypack.dev/confetti-js@0.0.18";
+
+let logger = [];
+let lastKeyTime = Date.now();
+
+const confettiSettings = {
+    target: "confetti-holder",
+    max: "80",
+    size: "1",
+    animate: true,
+    props: ["circle", "square", "triangle", "line"],
+    colors: [
+        [165, 104, 246],
+        [230, 61, 135],
+        [0, 199, 228],
+        [253, 214, 126]
+    ],
+    clock: "30",
+    rotate: true,
+    start_from_edge: false,
+    respawn: false
+};
+
+const confetti = new confettiJs(confettiSettings);
+
+function konamiCode(e) {
+    const key = e.key.toLowerCase();
+
+    if (Date.now() - lastKeyTime > 1000) {
+        logger = [];
+    }
+
+    logger.push(key);
+    lastKeyTime = Date.now();
+
+    if (
+        logger.join("") ===
+        "arrowuparrowuparrowdownarrowdownarrowleftarrowrightarrowleftarrowrightbaenter"
+    ) {
+        confetti.render();
+        document.removeEventListener("keydown", konamiCode);
+    }
+}
+
+document.addEventListener("keydown", konamiCode);
