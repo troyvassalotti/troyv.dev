b88b87a7173d16657cb0001aef6c895d656bfb90
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-08-02 19:08:28 -0400

    switched to dynamic imports in some cases

--- src/_includes/js/script.js
+++ src/_includes/js/script.js
@@ -1,5 +1,3 @@
-import confetti from 'https://cdn.skypack.dev/canvas-confetti';
-
 // add a class to the body for the sake of styling maybe
 document.body.classList.add("js");
 
@@ -7,41 +5,53 @@ let logger = [];
 let lastKeyTime = Date.now();
 
 function konamiCode(e) {
-    const key = e.key.toLowerCase();
+  const key = e.key.toLowerCase();
 
-    if (Date.now() - lastKeyTime > 1000) {
-        logger = [];
-    }
+  if (Date.now() - lastKeyTime > 1000) {
+    logger = [];
+  }
 
-    logger.push(key);
-    lastKeyTime = Date.now();
+  logger.push(key);
+  lastKeyTime = Date.now();
 
-    if (
-        logger.join("") ===
-        "arrowuparrowuparrowdownarrowdownarrowleftarrowrightarrowleftarrowrightbaenter"
-    ) {
-        var duration = 15 * 1000;
-        var animationEnd = Date.now() + duration;
-        var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  if (
+    logger.join("") ===
+    "arrowuparrowuparrowdownarrowdownarrowleftarrowrightarrowleftarrowrightbaenter"
+  ) {
+      import("https://cdn.skypack.dev/canvas-confetti").then((module) => {
+      let duration = 15 * 1000;
+      let animationEnd = Date.now() + duration;
+      const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-        function randomInRange(min, max) {
-            return Math.random() * (max - min) + min;
+      function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+
+      var interval = setInterval(function () {
+        var timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+          return clearInterval(interval);
         }
 
-        var interval = setInterval(function() {
-            var timeLeft = animationEnd - Date.now();
-
-            if (timeLeft <= 0) {
-                return clearInterval(interval);
-            }
-
-            var particleCount = 50 * (timeLeft / duration);
-            // since particles fall down, start a bit higher than random
-            confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
-            confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
-        }, 250);
-        document.removeEventListener("keydown", konamiCode);
-    }
+        var particleCount = 50 * (timeLeft / duration);
+        // since particles fall down, start a bit higher than random
+        module.default(
+          Object.assign({}, defaults, {
+            particleCount,
+            origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+          })
+        );
+        module.default(
+          Object.assign({}, defaults, {
+            particleCount,
+            origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+          })
+        );
+      }, 250);
+    });
+    document.removeEventListener("keydown", konamiCode);
+  }
 }
 
 document.addEventListener("keydown", konamiCode);
