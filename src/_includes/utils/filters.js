5ba80421f51d842204206f984afb2efa4cbf22cb
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-12-05 09:04:00 -0500

    update webmentions again

--- src/_includes/utils/filters.js
+++ src/_includes/utils/filters.js
@@ -24,7 +24,8 @@ module.exports = {
   htmlDateString: function (dateObj) {
     return DateTime.fromJSDate(dateObj).toFormat("yyyy-LL-dd");
   },
-  sanitize: function (content) {
+  sanitizeWebmention: function (entry) {
+    // Sanitize Webmention content
     // define which HTML tags you want to allow in the webmention body content
     // https://github.com/apostrophecms/sanitize-html#what-are-the-default-options
     const allowedHTML = {
@@ -34,14 +35,24 @@ module.exports = {
       },
     };
 
-    return content ? sanitizeHTML(content, allowedHTML) : "";
+    const { html, text } = entry.content;
+
+    if (html) {
+      // really long html mentions, usually newsletters or compilations
+      entry.content.value =
+        html.length > 2000
+          ? `mentioned this in <a href="${entry["wm-source"]}">${entry["wm-source"]}</a>`
+          : sanitizeHTML(html, allowedHTML);
+    } else {
+      entry.content.value = sanitizeHTML(text, allowedHTML);
+    }
+
+    return entry.value;
   },
   webmentionsForUrl: function (webmentions, url, allowedTypes) {
     // define which types of webmentions should be included per URL.
-    // possible values listed here:
     // https://github.com/aaronpk/webmention.io#find-links-of-a-specific-type-to-a-specific-page
     if (!allowedTypes) {
-      // all types
       allowedTypes = [
         "mention-of",
         "in-reply-to",
@@ -54,20 +65,21 @@ module.exports = {
     }
 
     // sort webmentions by published timestamp chronologically.
-    // swap a.published and b.published to reverse order.
     const orderByDate = (a, b) => new Date(b.published) - new Date(a.published);
 
     // only allow webmentions that have an author name
-    const checkRequiredFields = (entry) => {
-      const { author } = entry;
-      return !!author && !!author.name;
-    };
+    // const checkRequiredFields = (entry) => {
+    //   const { author } = entry;
+    //   return !!author && !!author.name;
+    // };
 
     // run all of the above for each webmention that targets the current URL
-    return webmentions
-      .filter((entry) => entry["wm-target"] === url)
-      .filter((entry) => allowedTypes.includes(entry["wm-property"]))
-      .filter(checkRequiredFields)
-      .sort(orderByDate);
+    return (
+      webmentions
+        .filter((entry) => entry["wm-target"] === url)
+        .filter((entry) => allowedTypes.includes(entry["wm-property"]))
+        // .filter(checkRequiredFields)
+        .sort(orderByDate)
+    );
   },
 };
