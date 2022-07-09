0e8b319b9a1a1b596a16af0ff8c2768dc389f011
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2022-07-09 15:10:17 -0400

    update lb data

--- src/music/index.11tydata.js
+++ src/music/index.11tydata.js
@@ -1,44 +1,83 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
-  const listenBrainzEndpoint = "https://api.listenbrainz.org/1/user/actionhamilton";
+  const listenBrainzEndpoint = "https://api.listenbrainz.org/1";
   const apiHeaders = {
     Authorization: `Token ${process.env.LISTENBRAINZ_TOKEN}`,
   };
 
   async function getNowPlaying() {
-    const response = await EleventyFetch(`${listenBrainzEndpoint}/playing-now`, {
-      duration: "1m",
-      type: "json",
-      fetchOptions: {
-        headers: apiHeaders,
-      },
-    });
+    try {
+      const response = await EleventyFetch(
+        `${listenBrainzEndpoint}/user/actionhamilton/playing-now`,
+        {
+          duration: "1m",
+          type: "json",
+          fetchOptions: {
+            headers: apiHeaders,
+          },
+        }
+      );
 
-    const { payload } = response;
-    const metadata = payload.listens[0].track_metadata;
-    const { artist_name: artist, track_name: song, release_name: release } = metadata;
+      const { payload } = response;
+      const metadata = payload.listens[0].track_metadata;
+      const { artist_name: artist, track_name: song, release_name: release } = metadata;
 
-    return { artist, song, release };
+      return { artist, song, release };
+    } catch (error) {
+      return false;
+    }
   }
 
   async function getMostRecentListens(count = 30) {
-    const response = await EleventyFetch(`${listenBrainzEndpoint}/listens?count=${count}`, {
-      duration: "10m",
-      type: "json",
-      fetchOptions: {
-        headers: apiHeaders,
-      },
-    });
+    try {
+      const response = await EleventyFetch(
+        `${listenBrainzEndpoint}/user/actionhamilton/listens?count=${count}`,
+        {
+          duration: "10m",
+          type: "json",
+          fetchOptions: {
+            headers: apiHeaders,
+          },
+        }
+      );
 
-    const { payload } = response;
-    const { listens } = payload;
-    const metadata = listens.map((track) => track.track_metadata);
+      const { payload } = response;
+      const { listens } = payload;
+      const metadata = listens.map((track) => track.track_metadata);
 
-    return metadata.map((track) => {
-      const { artist_name: artist, track_name: song, release_name: release } = track;
-      return { artist, song, release };
-    });
+      return metadata.map((track) => {
+        const { artist_name: artist, track_name: song, release_name: release } = track;
+        return { artist, song, release };
+      });
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async function getTopArtists(count = 10, range = "this_month") {
+    try {
+      const response = await EleventyFetch(
+        `${listenBrainzEndpoint}/stats/user/actionhamilton/artists?count=${count}&range=${range}`,
+        {
+          duration: "4w",
+          type: "json",
+          fetchOptions: {
+            headers: apiHeaders,
+          },
+        }
+      );
+
+      const { payload } = response;
+      const { artists } = payload;
+
+      return artists.map((artist) => {
+        const { artist_name: artist, listen_count: listens } = artist;
+        return { artist, listens };
+      });
+    } catch (error) {
+      return false;
+    }
   }
 
   return {
@@ -60,5 +99,6 @@ module.exports = async function () {
     ],
     nowPlaying: await getNowPlaying(),
     recentListens: await getMostRecentListens(),
+    topArtistsThisMonth: await getTopArtists(),
   };
 };
