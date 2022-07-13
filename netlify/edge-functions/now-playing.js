74f83db3379cd2b82773888f28ee9fd9657f17f3
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2022-07-12 22:48:59 -0400

    need to figure out why not all edge functions are working at the same time

---
+++ netlify/edge-functions/now-playing.js
@@ -0,0 +1,50 @@
+import { EleventyEdge } from "eleventy:edge";
+import precompiledAppData from "./_generated/eleventy-edge-app-data.js";
+
+const token = Deno.env.get("LISTENBRAINZ_TOKEN");
+
+const listenBrainzEndpoint = "https://api.listenbrainz.org/1";
+const headers = new Headers();
+headers.append("Authorization", "Token " + token);
+
+async function getNowPlaying() {
+  try {
+    const response = await fetch(`${listenBrainzEndpoint}/user/actionhamilton/playing-now`, {
+      headers,
+    });
+
+    const data = await response.json();
+
+    const { payload } = data;
+    const metadata = payload.listens[0].track_metadata;
+    const { artist_name: artist, track_name: song, release_name: release } = metadata;
+
+    return { artist, song, release };
+  } catch (error) {
+    return false;
+  }
+}
+
+export default async (request, context) => {
+  try {
+    let edge = new EleventyEdge("edge", {
+      request,
+      context,
+      precompiled: precompiledAppData,
+
+      // default is [], add more keys to opt-in e.g. ["appearance", "username"]
+      cookies: [],
+    });
+
+    const nowPlaying = await getNowPlaying();
+
+    edge.config((eleventyConfig) => {
+      eleventyConfig.addGlobalData("nowPlaying", nowPlaying);
+    });
+
+    return await edge.handleResponse();
+  } catch (e) {
+    console.log("ERROR", { e });
+    return context.next(e);
+  }
+};
