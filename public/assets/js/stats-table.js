efefbf70a3a6d9d146359ec238562f3f964a8f56
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2023-11-13 21:08:44 -0500

    Test out nvim with lazyvim

--- public/assets/js/stats-table.js
+++ public/assets/js/stats-table.js
@@ -1,9 +1,9 @@
 import { css, html, LitElement, nothing } from "lit";
 
 class StatsTable extends LitElement {
-	static get styles() {
-		return [
-			css`
+  static get styles() {
+    return [
+      css`
         :host {
           box-sizing: border-box;
           display: block;
@@ -69,88 +69,63 @@ class StatsTable extends LitElement {
           --colWidth: 40ch;
         }
       `,
-		];
-	}
+    ];
+  }
 
-	static get properties() {
-		return {
-			caption: { type: String },
-			headers: { type: String },
-		};
-	}
+  static get properties() {
+    return {
+      caption: { type: String },
+      headers: { type: String },
+    };
+  }
 
-	constructor() {
-		super();
-		this.caption = "";
-		this.headers = "";
-	}
+  constructor() {
+    super();
+    this.caption = "";
+    this.headers = "";
+  }
 
-	get data() {
-		return this.getAttribute("data");
-	}
+  get data() {
+    return this.getAttribute("data");
+  }
 
-	render() {
-		const parsedData = JSON.parse(this.data);
-		const headersList = this.headers.split(" ");
+  render() {
+    const parsedData = JSON.parse(this.data) ?? null;
+    const headersList = this.headers.split(" ");
 
-		return html`
-      <table>
-        ${
-			this.caption
-				? html` <caption>
-              ${this.caption}
-            </caption>`
-				: nothing
-		}
-        <colgroup>
-          ${
-			headersList.map((header) => {
-				return html` <col class="col--${header.toLowerCase()}" /> `;
-			})
-		}
-        </colgroup>
-        <thead>
-          <tr>
-            ${
-			headersList.map((header) => {
-				return html` <th id="${header.toLowerCase()}" scope="col">${header}</th> `;
-			})
-		}
-          </tr>
-        </thead>
-        <tbody>
-          ${
-			parsedData.map((track, index) => {
-				return html`
+    return parsedData
+      ? html`
+          <table>
+            ${this.caption
+              ? html` <caption>
+                  ${this.caption}
+                </caption>`
+              : nothing}
+            <colgroup>
+              ${headersList.map((header) => html`<col class="col--${header.toLowerCase()}" />`)}
+            </colgroup>
+            <thead>
               <tr>
-                <th
-                  id="order-${headersList[0].toLowerCase()}"
-                  headers="${headersList[0].toLowerCase()}"
-                  scope="row">
-                  ${index + 1}
-                </th>
-                ${
-					Object.keys(track).map((item, index) => {
-						return html`
-                    <td
-                      headers="order-${headersList[0].toLowerCase()} ${
-							headersList[
-								index
-							].toLowerCase()
-						}">
-                      ${track[item]}
-                    </td>
-                  `;
-					})
-				}
+                ${headersList.map((header) => html`<th id="${header.toLowerCase()}" scope="col">${header}</th>`)}
               </tr>
-            `;
-			})
-		}
-        </tbody>
-      </table>
-    `;
-	}
+            </thead>
+            <tbody>
+              ${parsedData.map(
+                (track, index) => html`
+                  <tr>
+                    <th id="order-${headersList[0].toLowerCase()}" headers="${headersList[0].toLowerCase()}" scope="row">${index + 1}</th>
+                    ${Object.keys(track).map(
+                      (item, index) => html`
+                        <td headers="order-${headersList[0].toLowerCase()} ${headersList[index].toLowerCase()}">${track[item]}</td>
+                      `,
+                    )}
+                  </tr>
+                `,
+              )}
+            </tbody>
+          </table>
+        `
+      : html`<p>Data not found.</p>`;
+  }
 }
-
 customElements.define("stats-table", StatsTable);
