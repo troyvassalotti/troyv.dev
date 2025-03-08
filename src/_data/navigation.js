83d097c3a32b25a163ee007a50fa4d96d4385436
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2025-03-08 11:02:44 -0500

    remove dead link

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -18,8 +18,4 @@ export default [
 	new Navigation("music"),
 	new Navigation("contact"),
 	new Navigation("links"),
-	new Navigation(
-		"likes",
-		"https://www.inoreader.com/stream/user/1005377372/tag/user-liked/view/html?t=Liked%20Feed&l=https%3A%2F%2Fwww.troyv.dev%2Ffavicons%2Fandroid-chrome-512x512.png&cs=m",
-	),
 ];
